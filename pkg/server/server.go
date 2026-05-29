d211aec83c41e6505dca4e16941ba1625008cca0
Patrik Lundin <patlu@sunet.se>
2026-05-29 09:29:09 +0200

    Do login page redirects with HX-Redirect for htmx
    When clicking a link in the UI with an expired session cookie I noticed
    I was redirected to a broken login page. This seems to be a result of
    hx-boost making htmx do the request, following the login page redirect
    and swapping the returned response into the body.
    
    To make this behave better detect if we are receiving a request sent by
    htmx and in that case handle the redirect by setting a HX-Redirect
    reponse header instead.
    
    Add test to check for the header as well.

--- pkg/server/server.go
+++ pkg/server/server.go
@@ -112,6 +112,7 @@ const (
 	// exclusion_violation: 23P01
 	pgExclusionViolation = "23P01"
 
+	authLoginPath                    = "/auth/login"
 	consolePath                      = "/console"
 	consoleSuperuserCacheNodes       = "/console/superuser/cache-nodes"
 	consoleSuperuserL4LBNodes        = "/console/superuser/l4lb-nodes"
@@ -3225,7 +3226,26 @@ func redirectToLoginPage(w http.ResponseWriter, r *http.Request, session *sessio
 		logger.Err(err).Msg("redirectToLoginPage: failed to save pending return-to in session")
 	}
 
-	validatedRedirect("/auth/login", w, r, http.StatusFound)
+	if r.Header.Get("HX-Request") != "" {
+		// This is a htmx request (probably due to the use of
+		// hx-boost). Instead of sending a normal redirect which would
+		// lead to htmx swapping the login page into the body instruct
+		// it to do a full reload of the whole page with a response header.
+		w.Header().Add("HX-Redirect", authLoginPath)
+
+		logger.Info().Msg("issuing HX-Redirect to login page")
+		// From https://htmx.org/headers/hx-redirect/
+		// ===
+		// Response headers are not processed on 3xx response codes.
+		// ===
+		_, err := fmt.Fprintf(w, "HX-Redirect header is set to '%s'", authLoginPath)
+		if err != nil {
+			logger.Err(err).Msg("redirectToLoginPage: unable to write HX-Redirect response")
+		}
+		return
+	}
+
+	validatedRedirect(authLoginPath, w, r, http.StatusFound)
 }
 
 func createLoginCacheKey(userID pgtype.UUID, expectedPasswordHash []byte, expectedSalt []byte, password string) string {
